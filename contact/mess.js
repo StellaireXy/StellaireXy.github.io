672493d508c60e1a7bb895c02796ea354c4a316a
StellaireXy <StellaireXy@users.noreply.github.com>
2018-05-13 21:52:09 +0200

    update

---
+++ contact/mess.js
@@ -0,0 +1,51 @@
+const clientPromise = stitch.StitchClientFactory.create('messager-yljif');
+
+let client;
+let db;
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+                    + " " + date.getHours() + seperator2 + date.getMinutes()
+                    + seperator2 + date.getSeconds();
+    return currentdate;
+}
+
+function displayCommentsOnLoad() {
+    clientPromise.then(stitchClient => {
+        client = stitchClient;
+        db = client.service('mongodb', 'mongodb-atlas').db('blog');
+        return client.login().then(displayComments)
+    });
+}
+
+function displayComments() {
+    db.collection('comments').find({}).limit(1000).execute().then(docs => {
+        var html = docs.map(c => "<div><font color='#0099FF' size='1'>" + c.time + '</font><br>' + c.comment + "</div>").join("");
+        document.getElementById("comments").innerHTML = html;
+    });
+}
+
+function addComment() {
+    var foo = document.getElementById("new_comment");
+    var timestamp = getNowFormatDate();
+    
+    if (foo.value.substring(0,3) === "DEL") {
+        timestamp = foo.value.substring(4);
+        db.collection("comments").deleteOne({time: timestamp}).then(displayComments);
+    }
+    else {
+        db.collection("comments").insertOne({owner_id : client.authedId(), time: timestamp, comment: foo.value}).then(displayComments);
+        foo.value = "";
+    }
+}
